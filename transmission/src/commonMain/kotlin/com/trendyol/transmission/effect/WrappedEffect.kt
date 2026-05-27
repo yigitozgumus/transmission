b3bb10ae6fe47ade0ae13a8213b4dd8911c186c1
Yigit Ozgumus <yigitozgumus1@gmail.com>
2026-05-28 00:28:57 +0300

    Introduce transmission envelope

--- transmission/src/commonMain/kotlin/com/trendyol/transmission/effect/WrappedEffect.kt
+++ transmission/src/commonMain/kotlin/com/trendyol/transmission/effect/WrappedEffect.kt
@@ -1,9 +1,25 @@
 package com.trendyol.transmission.effect
 
 import com.trendyol.transmission.Transmission
+import com.trendyol.transmission.router.TransmissionEnvelope
 import com.trendyol.transmission.transformer.request.Contract
 
 internal data class WrappedEffect(
-    val effect: Transmission.Effect,
-    val identity: Contract.Identity? = null
-)
+    val envelope: TransmissionEnvelope<Transmission.Effect>
+) {
+    constructor(
+        effect: Transmission.Effect,
+        identity: Contract.Identity? = null
+    ) : this(
+        TransmissionEnvelope(
+            payload = effect,
+            target = identity,
+        )
+    )
+
+    val effect: Transmission.Effect
+        get() = envelope.payload
+
+    val identity: Contract.Identity?
+        get() = envelope.target
+}
---
+++ transmission/src/commonMain/kotlin/com/trendyol/transmission/router/TransmissionEnvelope.kt
@@ -0,0 +1,10 @@
+package com.trendyol.transmission.router
+
+import com.trendyol.transmission.Transmission
+import com.trendyol.transmission.transformer.request.Contract
+
+internal data class TransmissionEnvelope<out T : Transmission>(
+    val payload: T,
+    val source: Contract.Identity? = null,
+    val target: Contract.Identity? = null,
+)
